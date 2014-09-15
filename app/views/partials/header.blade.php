6790708746d82e839098e9eb422fdef082ce30b7
Steven Wade <stevenwadejr@gmail.com>
2014-09-15 10:47:02 -0400

    Added Google Analytics

--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -14,6 +14,17 @@
     <meta property="og:description" content="UpstatePHP is a group of like minded professionals helping to build a better community, specifically in the Upstate of South Carolina." />
 
     {{ Orchestra\Asset::styles() }}
+
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-54800737-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
 </head>
 
 <body>
