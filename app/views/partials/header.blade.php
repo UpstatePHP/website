b58c295706a47a4f31148725d7a928d8d7cd082e
Steven Wade <stevenwadejr@gmail.com>
2014-09-15 08:48:11 -0400

    Added Open Graph meta data

--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -1,8 +1,17 @@
 <html>
 <!--<![endif]-->
 <head>
-    <meta name="viewport" content="width=device-width" />
-    <title></title>
+    <meta name="viewport" content="width=device-width" /></title>
+
+    <title>UpstatePHP</title>
+    <meta name="description" content="UpstatePHP is a group of like minded professionals helping to build a better community, specifically in the Upstate of South Carolina." />
+
+    <!-- Open Graph data -->
+    <meta property="og:title" content="UpstatePHP" />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="http://upstatephp.com/" />
+    <meta property="og:image" content="http://upstatephp.com/images/upstatephp-logo.jpg" />
+    <meta property="og:description" content="UpstatePHP is a group of like minded professionals helping to build a better community, specifically in the Upstate of South Carolina." />
 
     {{ Orchestra\Asset::styles() }}
 </head>
