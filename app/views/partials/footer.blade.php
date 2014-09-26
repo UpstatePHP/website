4fed524b7d792ac6e6d37888754adb0fd731fb8d
Steven Wade <stevenwadejr@gmail.com>
2014-09-26 14:11:12 -0400

    [Bug #2] fixed where footer social links didn't open in new window

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -3,19 +3,19 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="social-links pull-right">
-                        <a href="https://github.com/UpstatePHP" class="social-link github" title="Contribute on GitHub">
+                        <a href="https://github.com/UpstatePHP" class="social-link github" target="_blank" title="Contribute on GitHub">
                             @include('partials.svgs.github')
                             <div class="hexagon"></div>
                         </a>
-                        <a href="https://www.facebook.com/groups/upstatepug" class="social-link facebook" title="Join the group on Facebook">
+                        <a href="https://www.facebook.com/groups/upstatepug" class="social-link facebook" target="_blank" title="Join the group on Facebook">
                             @include('partials.svgs.facebook')
                             <div class="hexagon"></div>
                         </a>
-                        <a href="https://twitter.com/UpstatePHP" class="social-link twitter" title="Follow us on Twitter">
+                        <a href="https://twitter.com/UpstatePHP" class="social-link twitter" target="_blank" title="Follow us on Twitter">
                             @include('partials.svgs.twitter')
                             <div class="hexagon"></div>
                         </a>
-                        <a href="https://www.youtube.com/channel/UCnibunYYwTP3ZEJEqd-H7ug" class="social-link youtube" title="Subscribe to our channel on YouTube">
+                        <a href="https://www.youtube.com/channel/UCnibunYYwTP3ZEJEqd-H7ug" class="social-link youtube" target="_blank" title="Subscribe to our channel on YouTube">
                             @include('partials.svgs.youtube')
                             <div class="hexagon"></div>
                         </a>
