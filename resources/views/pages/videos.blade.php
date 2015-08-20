c35f0b4e64a74772f8bde8151017e5d70fd6503f
Steven Wade <stevenwadejr@gmail.com>
2015-08-20 08:26:41 -0400

    - Added card styles from Bootstrap 4 (with edits). - Updated styles and markup for videos. Each video on the video page is a card.

--- resources/views/pages/videos.blade.php
+++ resources/views/pages/videos.blade.php
@@ -4,24 +4,25 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-12">
-            <div class="card">
-                <div class="card-content hero">
-                    <div class="page-header">
-                        <h1>Videos</h1>
-                    </div>
+            <div class="card-deck-wrapper">
+                <div class="card-deck card-columns">
 
                     @foreach ($videos as $video)
-                        <div class="col-lg-4 video">
-                            <div class="thumbnail">
-                                <img src="{!! $video->thumbnail->url('medium') !!}" title="{!! $video->title !!}" />
-                                <div class="caption">
-                                    {!! $video->published_at->format('F j, Y') !!}
+                        <div class="col-lg-4 video card">
+                            <div>
+                                <img class="card-img-top" src="{!! $video->thumbnail->url('medium') !!}" title="{!! $video->title !!}" />
+                                <div class="card-block">
+                                    <p>
+                                        <h4>{!! $video->title !!}</h4>
+                                    </p>
+                                    <p>{!! $video->published_at->format('F j, Y') !!}</p>
                                 </div>
                                 <div class="media__body">
-                                    <h2>{!! $video->title !!}</h2>
-                                    <p>
-                                        <a href="https://www.youtube.com/watch?v={!! $video->video_id !!}" target="_blank"><span class="glyphicon glyphicon-play"></span></a>
-                                    </p>
+                                    <a href="https://www.youtube.com/watch?v={!! $video->video_id !!}" target="_blank">
+                                        <span class="play-circle">
+                                            <span class="glyphicon glyphicon-play"></span>
+                                        </span>
+                                    </a>
                                 </div>
                             </div>
                         </div>
