0b65bd8f02110ae5c0f4730178be1dbcc9c42abd
Steven Wade <stevenwadejr@gmail.com>
2015-08-26 20:47:39 -0400

    Scheduled a backup command to run daily at 12:01am

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,8 +22,8 @@ class Kernel extends ConsoleKernel {
 	 */
 	protected function schedule(Schedule $schedule)
 	{
-		$schedule->command('inspire')
-				 ->hourly();
+		$schedule->command('upstatephp:backup')
+			->dailyAt('00:01');
 	}
 
 }
